4c0f0bec98f8a6078e6b39cf2d01c2f9c6c34810
F. Sabre <fabien.sabre@gmail.com>
2020-01-29 11:24:18 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="docnetdb",
+    version="0.2",
+    license="MIT",
+    author="fsabre",
+    author_email="fabien.sabre@gmail.com",
+    description="A pure Python document and graph database engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fsabre/DocNetDB",
+    packages=setuptools.find_packages(),
+    keywords=["document", "graph", "database", "nosql", "pure python"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
